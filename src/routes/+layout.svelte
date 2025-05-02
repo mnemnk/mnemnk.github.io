cf1f66f6cc10133498b41f1d4b539036d150e720
Akira Ishino <akira@lumilab.jp>
2025-05-03 02:09:54 +0900

    feat: 🎸 set dark theme default

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
 <script>
   const { children } = $props()
+
+  $effect.pre(() => {
+    const key = 'SVELTEPRESS_DARK_MODE'
+    if (localStorage.getItem(key) === null) {
+        localStorage.setItem(key, 'on')
+    }
+  })
 </script>
 
 <!-- Leave this. Or you can add more content for your custom layout -->
